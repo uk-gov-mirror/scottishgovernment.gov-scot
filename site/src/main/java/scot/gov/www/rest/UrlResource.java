b291ce74536dbb3aac2ced358d337ba739d2859c
David Sinclair <david.sinclair@gov.scot>
2019-03-22 15:38:09 +0000

    MGS-5090 Add rest endpoints to expose metadata
    To support the APS registrations form we now expose
    some metadata:
    - topics
    - directorates
    - publication types
    - roles

--- site/src/main/java/scot/gov/www/rest/UrlResource.java
+++ site/src/main/java/scot/gov/www/rest/UrlResource.java
@@ -7,6 +7,8 @@ import org.hippoecm.hst.jaxrs.services.AbstractResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestBody;
+import scot.gov.www.rest.metadata.MetadataResource;
+import scot.gov.www.rest.metadata.MetadataResponse;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
